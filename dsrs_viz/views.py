e1131f72635e5ff0f7352ff24a8b8904c73dd0c9
Mohit Gupta <44348351+27guptamohit@users.noreply.github.com>
2020-08-18 18:07:41 -0500

    Added detail view class

--- dsrs_viz/views.py
+++ dsrs_viz/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http.response import HttpResponse
 from django.template import loader
 from django.views import View
@@ -9,6 +9,10 @@ from .models import (
     D3_react_js,
 )
 
+
+
+
+#----------------------------------------------------------
 # Function based view
 # def d3_js_list_view(request):
 #     d3_js_list = D3_js.objects.all()
@@ -18,20 +22,6 @@ from .models import (
 #     return HttpResponse(output)
 
 
-# def react_js_list_view(request):
-#     react_js_list = React_js.objects.all()
-#     template = loader.get_template('dsrs_viz/react_js_list.html')
-#     context = {'react_js_list': react_js_list}
-#     output = template.render(context)
-#     return HttpResponse(output)
-#
-#
-# def d3_react_js_list_view(request):
-#     d3_react_js_list = D3_react_js.objects.all()
-#     template = loader.get_template('dsrs_viz/d3_react_js_list.html')
-#     context = {'d3_react_js_list': d3_react_js_list}
-#     output = template.render(context)
-#     return HttpResponse(output)
 
 
 # Class based view
@@ -45,6 +35,33 @@ class D3JSList(View):
         )
 
 
+class D3JSDetail(View):
+
+    def get(self, request, pk):
+        d3_js = get_object_or_404(
+            D3_js,
+            pk=pk
+        )
+
+        return render(
+            request,
+            'dsrs_viz/d3_js_detail.html',
+            {'d3_js':d3_js}
+        )
+
+
+
+#----------------------------------------------------------
+# def react_js_list_view(request):
+#     react_js_list = React_js.objects.all()
+#     template = loader.get_template('dsrs_viz/react_js_list.html')
+#     context = {'react_js_list': react_js_list}
+#     output = template.render(context)
+#     return HttpResponse(output)
+#
+#
+
+
 class ReactJSList(View):
 
     def get(self, request):
@@ -55,6 +72,33 @@ class ReactJSList(View):
         )
 
 
+class ReactJSDetail(View):
+
+    def get(self, request, pk):
+        react_js = get_object_or_404(
+            React_js,
+            pk=pk
+        )
+
+        return render(
+            request,
+            'dsrs_viz/react_js_detail.html',
+            {'react_js': react_js}
+        )
+
+
+#----------------------------------------------------------
+
+# def d3_react_js_list_view(request):
+#     d3_react_js_list = D3_react_js.objects.all()
+#     template = loader.get_template('dsrs_viz/d3_react_js_list.html')
+#     context = {'d3_react_js_list': d3_react_js_list}
+#     output = template.render(context)
+#     return HttpResponse(output)
+
+
+
+
 class D3ReactJSList(View):
 
     def get(self, request):
@@ -63,3 +107,19 @@ class D3ReactJSList(View):
             'dsrs_viz/d3_react_js_list.html',
             {'d3_react_js_list': D3_react_js.objects.all()}
         )
+
+
+class D3ReactJSDetail(View):
+
+    def get(self, request, pk):
+        d3_react_js = get_object_or_404(
+            D3_react_js,
+            pk=pk
+        )
+
+        return render(
+            request,
+            'dsrs_viz/d3_react_js_detail.html',
+            {'d3_react_js': d3_react_js}
+        )
+
